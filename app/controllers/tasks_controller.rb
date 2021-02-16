bd1699fbb02d27c5c44966bb9451c4cbc527cf5e
Annagloria Santello <annag.santello@gmail.com>
2021-02-16 16:40:50 +0100

    update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,4 +7,33 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new # needed to instantiate the form_for
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    # no need for app/views/restaurants/create.html.erb
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    # no need for app/views/restaurants/edit.html.erb
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
+
